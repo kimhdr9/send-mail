1b4e37373906d5a9d58cb1d4d3e6db4c62deda9f
kim do <kimhdr9@gmail.com>
2023-11-01 07:05:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Programme en python pour émettre des mails
+
+### alimenter les variables d'environnement dans le fichier 
+
+export SMTP_HOST=""
+export SMTP_PORT=587
+export SMTP_FROM=""
+export SMTP_TO=""
+
+### copier .environnements.bash.example en .environnements.bash
+
+### remplacer la partie droite par des valeurs
+### puis . .environnements.bash
+
+> python simple_email.py
+> entrer le mot de passe de votre adresse mail
---
+++ simple_email.py
@@ -0,0 +1,42 @@
+import getpass
+import smtplib
+import os
+
+HOST=os.getenv('SMTP_HOST')
+PORT=os.getenv('SMTP_PORT')
+FROM=os.getenv('SMTP_FROM')
+TO=os.getenv('SMTP_TO')
+PASSWORD=getpass.getpass("Entrez le mot de passe :")
+
+MESSAGE = """ Subject : Mail sent using python
+Hi gmail,
+
+This email is a test
+
+Cheers
+
+"""
+
+smtp=smtplib.SMTP(HOST,PORT)
+
+status_code, reponse = smtp.ehlo()
+
+print(f" Retour serveur : {status_code} {reponse}")
+
+status_code, reponse = smtp.starttls()
+
+print(f" Starting TLS connection : {status_code} {reponse}")
+
+status_code, reponse = smtp.login(FROM,PASSWORD)
+
+print(f" Logging in : {status_code} {reponse}")
+
+try :
+    smtp.sendmail(FROM,TO,MESSAGE)
+except ValueError :
+    status_code,reponse = smtplib.SMTPDataError()
+    print(f"code {status_code} message : {reponse}")
+finally :
+    # quitte smtp
+    smtp.quit()
+
